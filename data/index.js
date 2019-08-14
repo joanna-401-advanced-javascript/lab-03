81c68ce332917c4d0cb1d606eabbcaee5325d12e
joannaarroyo <joliuac@gmail.com>
2019-08-14 15:22:51 -0700

    changed object first and last name

--- data/index.js
+++ data/index.js
@@ -1,15 +1,20 @@
 'use strict';
 
 const FileEdit = require('../edit-file');
+const faker = require('faker');
 
 const currentEdit = new FileEdit();
 
-console.log(__dirname);
+// console.log(__dirname);
 
 currentEdit.readFile(`${__dirname}/person.json`, (error, fileContents) => {
     if (error){
         console.error(error);
     } else {
-        console.log(fileContents);
+        let currentObject = JSON.parse(fileContents);
+        console.log('current object', currentObject);
+        currentObject.firstName = faker.name.firstName();
+        currentObject.lastName = faker.name.lastName();
+        console.log('new object', currentObject);
     }
 })
